a84add63b97e2f6cf924d5cfaef3b86d6ee1bfe0
MOHAMED ABDULLAHI MOHAMUD <44861583+ShurieJr@users.noreply.github.com>
2024-05-02 11:44:31 +0300

    Increment and decrement operators

---
+++ 6. Increment & Decrement/IncrementAndDecrement.js
@@ -0,0 +1,18 @@
+//Increment and decrement operators
+
+let salary = 1000;
+
+console.log(salary++);  //1000             //post Increment
+console.log(salary);  // 1001
+
+let age = 40;
+console.log(++age); // 41    // pre Increment
+
+
+
+let grade = 30;
+console.log(--grade); //  pre decrement  // 29
+
+let test = 90;
+console.log(test--); // post decrement  // 90
+console.log(test); // 89
