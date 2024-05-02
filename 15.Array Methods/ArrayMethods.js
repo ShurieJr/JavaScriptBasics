dc995df4f730af33472d03f6b70c968f1841deb8
MOHAMED ABDULLAHI MOHAMUD <44861583+ShurieJr@users.noreply.github.com>
2024-05-02 14:55:15 +0300

    arrray methods

--- 15.Array Methods/ArrayMethods.js
+++ 15.Array Methods/ArrayMethods.js
@@ -15,3 +15,34 @@
     let arr6 = [4, 5, 6];
     let arr7 = arr5.concat(arr6);
     console.log(arr7);
+    //This concat() method can do even more! We can use it to add values as well. We can
+  //add a single value, or we can comma-separate multiple values:
+let arr8 = arr7.concat(7, 8, 9,10, 11);
+console.log(arr8);
+
+//pop()
+arr8 = arr7.pop();  //Removing the last element
+//shift()   
+arr8.shift(); //Removing the first element
+
+//Remember splice()? This is a very special method because we can also use it to
+//delete values. We specify the index from where we want to start deleting, and then
+//the number of elements we want to delete.
+  arr8.splice(1, 3);    //splice() as delete , and it takes two parameters (begin, end); 
+
+
+  //delete
+  delete arr8[0];  //  <1 empty item>
+
+  //find()
+  let findValue = arr8.find(function(e) { return e=== 7});
+  console.log(findValue);
+
+  //indexOf()
+  console.log(arr8.indexOf(10));
+
+  //replace(,)
+  arr8.replace(10, 20)
+
+  //reverse()
+  arr8.reverse();
