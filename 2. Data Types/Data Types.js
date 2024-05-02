aa89f7f1e3dff6e8bde0a06ebcbd3445faa3c902
MOHAMED ABDULLAHI MOHAMUD <44861583+ShurieJr@users.noreply.github.com>
2024-05-02 11:28:36 +0300

    data types

---
+++ 2. Data Types/Data Types.js
@@ -0,0 +1,11 @@
+//Data types in javascript
+
+console.log(typeof 23); //number
+console.log(typeof 'mohamed') //string
+console.log(typeof true); //boolean
+console.log(typeof "Ali"); //string
+console.log(typeof 2.5); //number
+console.log(typeof NaN); //number
+console.log(typeof undefined);   //undefined
+console.log(typeof {});   //object
+console.log(typeof null); //object
