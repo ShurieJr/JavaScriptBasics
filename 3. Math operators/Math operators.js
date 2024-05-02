e567ad85d17bce46b78f0b9d998225cdbca45157
MOHAMED ABDULLAHI MOHAMUD <44861583+ShurieJr@users.noreply.github.com>
2024-05-02 11:33:47 +0300

    math operators

---
+++ 3. Math operators/Math operators.js
@@ -0,0 +1,20 @@
+// Math operators 
+ // + - * / %  **
+
+ console.log("Math operators");
+ console.log(5 + 5);  //addition
+
+ console.log( 5 - 2); // subtraction
+
+ console.log(5 * 2 ); // multiplication
+
+ console.log(5 / 5 ); // division
+
+ console.log(10 % 2) ; //modulas operator
+
+ console.log(5 ** 2); // power
+
+ //when (+) operator is used in string it is a concatenation operator
+ const firstName = 'MOhamed';
+const lastName = 'Abduallahi';
+console.log(firstName + ' ' + lastName);
