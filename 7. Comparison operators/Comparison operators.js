0b16514e92089ef784deaccd8f327022c87e33f2
MOHAMED ABDULLAHI MOHAMUD <44861583+ShurieJr@users.noreply.github.com>
2024-05-02 11:55:39 +0300

    comparison operators

---
+++ 7. Comparison operators/Comparison operators.js
@@ -0,0 +1,18 @@
+//Comparison operators
+// > >= < <= == !=
+
+let aliAge = 20 , gediAge= 45;
+console.log(aliAge > gediAge);  //false
+console.log(aliAge >= gediAge);  //false
+
+console.log(aliAge < gediAge);  //true
+console.log(aliAge <= gediAge); // true
+
+console.log(aliAge == gediAge); //false
+console.log(aliAge != gediAge);  //true
+console.log(aliAge === gediAge);  //false  //Strict equality operator
+
+
+console.log('23' === 23);  //false
+console.log('23' == 23);  //true
+
