2dbc928a397cd638706407b363c6db079fafd867
MOHAMED ABDULLAHI MOHAMUD <44861583+ShurieJr@users.noreply.github.com>
2024-05-02 11:19:21 +0300

    changed

---
+++ Variables/variables.js
@@ -0,0 +1,13 @@
+//Declaring variables   let , var , const
+
+var firstName = "Mohamed";
+let lastName = " Abdullahi";
+
+const age = 28;
+
+console.log('Name: ' , firstName + lastName , '\n' , 'Age: ' , age  );
+
+//VALUES & variables
+console.log('javascript: ');  //value
+console.log(23); //value
+console.log(age); //variable
