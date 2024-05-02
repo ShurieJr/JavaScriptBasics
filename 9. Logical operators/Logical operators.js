55c0753d46081d320f83c625a8e60963cfb5e2f5
MOHAMED ABDULLAHI MOHAMUD <44861583+ShurieJr@users.noreply.github.com>
2024-05-02 12:09:53 +0300

    Logical operators

---
+++ 9. Logical operators/Logical operators.js
@@ -0,0 +1,11 @@
+//Logical operators 
+// AND ,  OR  , NOT
+
+let x =10 , y = 30 , z = 40 ;
+
+console.log(x > y && y < z );  //false
+
+console.log(x < y || y > z ); //true
+
+let isMarked = false;
+console.log(!isMarked); //true
