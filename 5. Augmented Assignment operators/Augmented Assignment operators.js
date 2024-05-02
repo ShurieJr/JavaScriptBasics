73d3f3016f67ddd5ff3b8679f414896874dbe305
MOHAMED ABDULLAHI MOHAMUD <44861583+ShurieJr@users.noreply.github.com>
2024-05-02 11:39:09 +0300

    augmented Assignment operators

---
+++ 5. Augmented Assignment operators/Augmented Assignment operators.js
@@ -0,0 +1,17 @@
+//Augmented Assignment operators
+let test = "java";
+
+test += "Script";
+
+console.log(test);
+
+let age = 30;
+
+age += 10;
+
+console.log(age);
+
+let salary = 900;
+
+salary -= 100;
+
