cdb15631a023c44ba00f8f655832bacaf286acd6
MOHAMED ABDULLAHI MOHAMUD <44861583+ShurieJr@users.noreply.github.com>
2024-05-02 12:24:01 +0300

    if else if

---
+++ 11. if else if statement/if else if statement.js
@@ -0,0 +1,16 @@
+// if else if statement
+let grade = 90;
+
+if(grade >=1 && grade <= 100){
+    if(grade >= 90)
+        console.log("A");
+    else if(grade >= 80)
+        console.log("B");
+    else if(grade >= 70)
+        console.log("C");
+    else
+        console.log("F");
+}
+else{
+    console.log("Invalid grade number");
+}
