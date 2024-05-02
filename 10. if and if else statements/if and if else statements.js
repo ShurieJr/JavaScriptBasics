48cc8e104701ea0096ebb35bb53b5db397464a41
MOHAMED ABDULLAHI MOHAMUD <44861583+ShurieJr@users.noreply.github.com>
2024-05-02 12:18:07 +0300

    if and if else statements

---
+++ 10. if and if else statements/if and if else statements.js
@@ -0,0 +1,13 @@
+//if and if else statements
+//if statement
+
+let active = false;
+
+if(!active) 
+    console.log("Inactive!");
+
+
+if(active)
+    console.log("Active!");
+else
+    console.log("Inactive!");
